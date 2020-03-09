42371afad36149b4a8317b58e59be85aaedf6672
arpit2603 <arpitkumar.namdeo@dotsquares.com>
2020-03-09 16:49:54 +0530

    create controllers and models

---
+++ src/main/java/com/masters/model/Classes.java
@@ -0,0 +1,30 @@
+package com.masters.model;
+
+import java.util.List;
+
+public class Classes {
+	
+	private long id;
+	private String name;
+	private List<Subject> subjectList;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Subject> getSubjectList() {
+		return subjectList;
+	}
+	public void setSubjectList(List<Subject> subjectList) {
+		this.subjectList = subjectList;
+	}
+	
+}
